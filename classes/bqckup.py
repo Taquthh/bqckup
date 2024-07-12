29b561092481df634be3d841fe6c3a7828186028
Taquthh <quthbiofficial@gmail.com>
2024-07-12 11:17:12 +0800

    Inii

--- classes/bqckup.py
+++ classes/bqckup.py
@@ -158,60 +158,63 @@ class Bqckup:
             
             #File Backup        
             compressed_file = os.path.join(tmp_path, f"{int(time.time())}.tar.gz")
-
-            print(f"Compressing {backup['path'][0]} for {backup['name']}")
-
-            log_compressed_files = Log().write({
-                "name": backup['name'],
-                "file_path": compressed_file,
-                "description": "File backup is in progress...",
-                "description": f"File backup is in progress: {os.path.basename(compressed_file)}",
-                "type": Log.__FILES__,
-                "storage": backup['options']['storage']
-            })
-            
             compressed_file = Tar().compress(backup.get('path'), compressed_file)
+            
+            print(f"Compressing {backup['path'][0]} for {backup['name']}")
+            
+            last_log = self.get_last_log(backup['name'])
+            if last_log:
+                last_backup_size = last_log.file_size
+                if last_backup_size is not None:
+                    current_file_size = os.stat(compressed_file).st_size
+                    compressed_filename = os.path.basename(compressed_file)
 
-            Log().update(file_size=os.stat(compressed_file).st_size).where(Log.id == log_compressed_files.id).execute()
-
-            if os.path.exists(compressed_file):
-                current_file_size = os.stat(compressed_file).st_size
-                Log().update(file_size=current_file_size).where(Log.id == log_compressed_files.id).execute()
-                Log().update_status(log_compressed_files.id, Log.__SUCCESS__, "File Backup Success")
-                print(f"Backup for {backup['name']} completed: {os.path.basename(compressed_file)}")
-
-                last_log = self.get_last_log(backup['name'])
-                if last_log:
-                    last_backup_size = last_log.file_size
-                    if last_backup_size is not None:
-                        
-                        if current_file_size == last_backup_size:
-                            print(f"\nCurrent file size: {current_file_size}")
-                            print(f"Last backup size: {last_backup_size}")
-                            from lib.notifications.discord import send_notification
-                            compressed_filename = os.path.basename(compressed_file)
-                        webhook_url = Config().read('notification', 'discord_webhook_url')
-                        if webhook_url:
-                                    send_notification({
-                                                        "embeds": [{
-                                                            "title": "Bqckup File Failed",
-                                                            "description": "This is an automated notification to inform you that the bqckup has failed.",
-                                                            "color": 15548997,
-                                                            "fields": [
-                                                            {"name": "Date",  "value": get_today(format="%d-%B-%Y"), "inline":True},     
-                                                            {"name": "Name", "value": backup.get('name'), "inline": True},
-                                                            {"name": "Server IP", "value": get_server_ip(), "inline": True},
-                                                            {"name": "File Backup", "value": compressed_filename, "inline": True}, 
-                                                            {"name": "Details", "value": "Please make sure, your set the right file for this backup", "inline": False}
-                                                            ],
-                                                            "footer": {"text": "If this was a mistake, please create issue here: https://github.com/bqckup/bqckup"}
-                                                        }]
-                                                    })
-                                    return False
-
-                        Log().update(file_size=current_file_size).where(Log.id == log_compressed_files.id).execute()
-                        Log().update_status(log_compressed_files.id, Log.__SUCCESS__, "File Backup Success")
+                if last_backup_size is None or current_file_size != last_backup_size:
+                    # Log file backup in progress
+                    log_compressed_files = Log().write({
+                        "name": backup['name'],
+                        "file_path": compressed_file,
+                        "description": "File backup is in progress...",
+                        "type": Log.__FILES__,
+                        "storage": backup['options']['storage']
+                    })
                     
+                    # Compress the file
+                    compressed_file = Tar().compress(backup.get('path'), compressed_file)
+
+                    # Update log with compressed file size
+                    Log().update(file_size=os.stat(compressed_file).st_size).where(Log.id == log_compressed_files.id).execute()
+
+                    if os.path.exists(compressed_file):
+                        current_file_size = os.stat(compressed_file).st_size
+                        Log().update(file_size=current_file_size).where(Log.id == log_compressed_files.id).execute()
+                        Log().update_status(log_compressed_files.id, Log.__SUCCESS__ == Log.__FILES__, "File Backup Success")
+                else:
+                    # Handle case where current_file_size == last_backup_size
+                    print(f"Backup file name: {compressed_filename}")
+                    print(f"\nCurrent file size: {current_file_size}")
+                    print(f"Last backup size: {last_backup_size}")
+                    from lib.notifications.discord import send_notification
+                    webhook_url = Config().read('notification', 'discord_webhook_url')
+                    if webhook_url:
+                        compressed_filename = os.path.basename(compressed_file)
+                        send_notification({
+                            "embeds": [{
+                                "title": "Bqckup File Failed",
+                                "description": "This is an automated notification to inform you that the bqckup has failed.",
+                                "color": 15548997,
+                                "fields": [
+                                    {"name": "Date",  "value": get_today(format="%d-%B-%Y"), "inline":True},     
+                                    {"name": "Name", "value": backup.get('name'), "inline": True},
+                                    {"name": "Server IP", "value": get_server_ip(), "inline": True},
+                                    {"name": "File Backup", "value": compressed_filename, "inline": True}, 
+                                    {"name": "Details", "value": "Please make sure, your set the right file for this backup", "inline": False}
+                                ],
+                                "footer": {"text": "If this was a mistake, please create issue here: https://github.com/bqckup/bqckup"}
+                            }]
+                        })
+                            
+
             #Database
             if backup.get('database'):
                 sql_path = os.path.join(tmp_path, f"{int(time.time())}.sql.gz")
